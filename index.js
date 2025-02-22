0cc4453a45cfaab5a15c1af065126d452609cc20
agawi88 <agnessn@icloud.com>
2025-02-23 00:04:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.get('/Users/:Username', passport.authenticate('jwt', { session: false }), as
 
 // Add a user (cannot have passport-authentication in order to allow users to register)
 app.post('/users', [
- // check('Username', 'Username is required').isLength({ min: 5 }),
+  check('Username', 'Username is required').isLength({ min: 5 }),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
   check('Email', 'Email does not appear to be valid').isEmail()
