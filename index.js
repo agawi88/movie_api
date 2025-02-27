b763edd3a4d9bce4c24e578939a9276b89b0b193
agawi88 <agnessn@icloud.com>
2025-02-27 14:04:23 +0100

    Updated index.js
    deleted code for origin restriction as it is already restricted by a code on line 34

--- index.js
+++ index.js
@@ -42,19 +42,6 @@ app.use(morgan('combined'));
 // Static request
 app.use(express.static('public'));
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
 app.use(morgan("combined", { stream: accessLogStream }));
 
