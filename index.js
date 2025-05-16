87c0fedfc0e0901e6ff34f665a458ab221250e41
agawi88 <agnessn@icloud.com>
2025-05-17 00:59:15 +0200

    updated inde.js
    trying a different cors code out as the errors in console remain

--- index.js
+++ index.js
@@ -31,7 +31,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+/* app.use(cors());*/
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.options('*', cors());
 
 require('./auth')(app);
