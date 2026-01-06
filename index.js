2c7c41a2afdedfcc9ccc563983462ff6dc15090f
Agnieszka <agnessn@icloud.com>
2026-01-06 13:38:47 +0100

    updated index.js
    added two new urls to accepted origins

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ let allowedOrigins =
   'http://localhost:1234',
   'https://gbmoviesflix.netlify.app',
   'http://localhost:4200',
-  'https://gb-movies-api-cab43a70da98.herokuapp.com/'
+  'https://gb-movies-api-cab43a70da98.herokuapp.com/',
+  'https://agawi88.github.io/myFlix-Angular-client/welcome'
 ];
 
 dotenv.config();
